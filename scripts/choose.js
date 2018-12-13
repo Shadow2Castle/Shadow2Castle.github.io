64a9877289d6af2e8a48aec1e7e2abe840a5f658
Shadow2Castle <uoxoa@student.kit.edu>
2018-12-13 18:02:59 +0100

    jump to random pages

--- scripts/choose.js
+++ scripts/choose.js
@@ -12,6 +12,30 @@ require(['./shake'], function(Shake) {
   function shakeEventDidOccur () {
 
       //put your own code here etc.
-      alert('shake!');
+      var x = Math.floor(Math.random() * 10) + 1;
+      switch(x) {
+        case 1: window.location.href = "games/overwatch.html";
+        break;
+        case 2: window.location.href = "games/hearthstone.html";
+        break;
+        case 3: window.location.href = "games/hots.html";
+        break;
+        case 4: window.location.href = "games/destiny.html";
+        break;
+        case 5: window.location.href = "games/warframe.html";
+        break;
+        case 6: window.location.href = "games/trove.html";
+        break;
+        case 7: window.location.href = "games/brawlhalla.html";
+        break;
+        case 8: window.location.href = "games/terraria.html";
+        break;
+        case 9: window.location.href = "games/rainbow.html";
+        break;
+        case 10: window.location.href = "games/crew.html";
+        break;
+        default:
+        break;
+      }
   }
 });
